e20b9a806fee375104936c449c10004f39cd9fba
pallavithorat <thoratpallavi2000@gmail.com>
2025-03-06 15:10:29 -0800

    made base URL dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,6 @@
-export const BASE_URL = "/api";
+//Production
+//export const BASE_URL = "/api";
+
+//dev
+
+export const BASE_URL = location.hostname ==="localhost"? "http://localhost:7777" : "/apis";
